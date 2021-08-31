a5ecdd237d65c8c7611e480666a7f7741fa20417
Brandon James Smylie <bsmylie02@qub.ac.uk>
2021-08-31 01:25:38 +0100

    Temporary env file added

--- app/app.py
+++ app/app.py
@@ -1,12 +1,28 @@
+from app import config as cfg
 import pymysql
 from hashids import Hashids
 from flask import Flask, render_template, request, flash, redirect, url_for
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
+
+from os import environ, path
+from dotenv import load_dotenv
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '../.env'))
+
+TESTING = True
+DEBUG = True
+FLASK_ENV = 'development'
+DB_HOST = environ.get('DB_HOST')
+DB_PASS = environ.get('DB_PASS')
+DB_USER = environ.get('DB_USER')
+SECRET_KEY = environ.get('SECRET_KEY')
 
 app = Flask(__name__)
 # Load config file.
-app.config.from_pyfile('config.py')
+app.config = cfg
 cors = CORS(app)
+
 
 
 def get_db_connection():
